0d8c631e73030aeeb712ff812994f02ce6a995a3
Kevin Brewster <kbrewster81@gmail.com>
2026-08-07 16:03:14 -0400

    First commit

--- movies.js
+++ movies.js
@@ -1,172 +1,109 @@
-let books;
+let movies;
 
-async function renderBooks(filter) {
-  const booksWrapper = document.querySelector(".books");
+async function renderMovies(filter) {
+  const moviesWrapper = document.querySelector(".movies");
 
-  booksWrapper.classList += " books__loading";
+  moviesWrapper.classList += " movies__loading";
 
-  if (!books) {
-    books = await getBooks();
+  if (!movies) {
+    movies = await getMovies();
   }
 
-  booksWrapper.classList.remove("books__loading");
+  moviesWrapper.classList.remove("movies__loading");
 
   if (filter === "SORT") {
-    const filteredBooks = books;
+    const filteredMovies = movies;
   }
   if (filter === "LOW_TO_HIGH") {
-    const filteredBooks = books.sort(
+    const filteredMovies = movies.sort(
       (a, b) =>
-        (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice),
+        (a.year  - b.year),
     );
-  } else if (filter === "HIGH_TO_LOW") {
-    const filteredBooks = books.sort(
+  } 
+  else if (filter === "HIGH_TO_LOW") {
+    const filteredMovies = movies.sort(
       (a, b) =>
-        (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice),
+        (b.year - a.year),
     );
-  } else if (filter === "RATING") {
-    const filteredBooks = books.sort((a, b) => b.rating - a.rating);
-  }
 
-  const booksHtml = books
-    .map((book) => {
-      return `<div class="book">
-       <figure class="book__img--wrapper">
-         <img class="book__img" src="${book.url}" alt="">
+  const moviesHtml = movies
+    .map((movie) => {
+      return `<div class="movie">
+       <figure class="movie__img--wrapper">
+         <img class="movie__img" src="${movie.url}" alt="">
        </figure>
-       <div class="book__title">
-        ${book.title}
+       <div class="movie__title">
+        ${movie.title}
        </div>
-       <div class="book__ratings">
-        ${ratingsHTML(book.rating)}
+       <div class="movie__ratings">
+        ${ratingsHTML(movie.rating)}
        </div>
-       <div class="book__price">
-          ${priceHTML(book.originalPrice, book.salePrice)}
+       <div class="movie__price">
+          ${priceHTML(movie.originalPrice, movie.salePrice)}
       </div>
      </div>`;
     })
     .join("");
 
-  booksWrapper.innerHTML = booksHtml;
+  moviesWrapper.innerHTML = moviesHtml;
 }
 
-function priceHTML(originalPrice, salePrice) {
-  if (!salePrice) {
-    return `$${originalPrice.toFixed(2)}`;
-  } else if (salePrice) {
-    return `<span class="book__price--normal">$${originalPrice.toFixed(2)}</span> $${salePrice.toFixed(2)}`;
-  }
+
+
+function filterMovies(event) {
+  renderMovies(event.target.value);
 }
 
-function filterBooks(event) {
-  renderBooks(event.target.value);
-}
-
-function ratingsHTML(rating) {
-  let ratingHTML = "";
-  for (let i = 0; i < Math.floor(rating); i++) {
-    ratingHTML += '<i class="fas fa-star"></i>';
-  }
-  if (!Number.isInteger(rating)) {
-    ratingHTML += '<i class="fas fa-star-half-alt"></i>';
-  }
-  return ratingHTML;
-}
 
 // FAKE DATA
-function getBooks() {
+
+function getMovies() {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve([
         {
           id: 1,
-          title: "Crack the Coding Interview",
-          url: "assets/crack-the-coding-interview.png",
-          originalPrice: 49.95,
-          salePrice: 14.95,
-          rating: 4.5,
+          title: "The Terminator",
+        
+          Year: "1984",
+          Type: "movie",
         },
         {
           id: 2,
-          title: "Atomic Habits",
-          url: "assets/atomic-habits.jpg",
-          originalPrice: 39,
-          salePrice: 19.95,
-          rating: 5,
+          title: "Terminator 2: Judgment Day",
+          url: "assets/Terminator 2.jpg",
+          Year: "1991",
+          Type: "movie",
         },
         {
           id: 3,
-          title: "Deep Work",
-          url: "assets/deep-work.jpeg",
-          originalPrice: 29,
-          salePrice: 12,
-          rating: 5,
+          title: "Terminator 3: Rise of the Machines",
+          url: "assets/Terminator 3.jpg",
+          Year: "2003",
+          Type: "movie",
         },
         {
           id: 4,
-          title: "The 10X Rule",
-          url: "assets/book-1.jpeg",
-          originalPrice: 44,
-          salePrice: 19,
-          rating: 4.5,
+          title: "The Terminator-Sarah Connor Chronicles",
+          url: "assets/The Terminator-Sarah Connor Chronicles.jpg",
+          Year: "2008",
+          Type: "movie",
         },
         {
           id: 5,
-          title: "Be Obsessed Or Be Average",
-          url: "assets/book-2.jpeg",
-          originalPrice: 32,
-          salePrice: 17,
-          rating: 4,
+          title: "Terminator Salvation",
+          url: "assets/Terminator Salvation.jpg",
+          Year: "2009",
+          Type: "movie",
         },
         {
           id: 6,
-          title: "Rich Dad Poor Dad",
-          url: "assets/book-3.jpeg",
-          originalPrice: 70,
-          salePrice: 12.5,
-          rating: 5,
-        },
-        {
-          id: 7,
-          title: "Cashflow Quadrant",
-          url: "assets/book-4.jpeg",
-          originalPrice: 11,
-          salePrice: 10,
-          rating: 4,
-        },
-        {
-          id: 8,
-          title: "48 Laws of Power",
-          url: "assets/book-5.jpeg",
-          originalPrice: 38,
-          salePrice: 17.95,
-          rating: 4.5,
-        },
-        {
-          id: 9,
-          title: "The 5 Second Rule",
-          url: "assets/book-6.jpeg",
-          originalPrice: 35,
-          salePrice: 14.95,
-          rating: 2,
-        },
-        {
-          id: 10,
-          title: "Your Next Five Moves",
-          url: "assets/book-7.jpg",
-          originalPrice: 40,
-          salePrice: 9.95,
-          rating: 4,
-        },
-        {
-          id: 11,
-          title: "Mastery",
-          url: "assets/book-8.jpeg",
-          originalPrice: 30,
-          salePrice: 12.95,
-          rating: 4.5,
+          title: "Terminator Genisys",
+          url: "assets/Terminator Genisys.jpg",
+          Year: "2015",
+          Type: "movie",
         },
       ]);
-    }, 1000);
   });
-}
+
+})
